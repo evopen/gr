3702c9dc36ab0819fb95db2e1f74143e93070892
evopen <520dhh@gmail.com>
2020-04-12 13:10:31 +0800

    setting up vulkan

---
+++ cpp/gpu-offscreen/src/pch.h
@@ -0,0 +1,13 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <set>
+#include <stdexcept>
+#include <vector>
---
+++ cpp/gpu-offscreen/src/shaders/compile.bat
@@ -0,0 +1,4 @@
+glslc.exe shader.vert -o vert.spv
+glslc.exe shader.frag -o frag.spv
+glslc.exe trace.comp -o trace.spv
+pause
